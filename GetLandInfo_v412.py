4d81a22bb2ca215f409bbcaef93693e68a95cee9
don.liao <44646592+DonLiao1207@users.noreply.github.com>
2023-04-12 10:48:52 +0800

    Update and rename GetLandInfo.py to GetLandInfo_v412.py

--- GetLandInfo_v412.py
+++ GetLandInfo_v412.py
@@ -14,22 +14,23 @@ year_month_day_time = now.strftime("%Y_%m_%d")
 # 設定 API 網址與 POST 資料
 
 Lcdetypename = {
-            "lcde_1" : "本國人",
-            "lcde_2" : "外國人",
-            "lcde_3" : "國有",
-            "lcde_4" : "省市",
-            "lcde_5" : "縣市",
-            "lcde_6" : "鄉鎮市",
-            "lcde_7" : "本國私法人",
-            "lcde_8" : "外國法人",
-            "lcde_9" : "祭祀公業",
-            "lcde_a" : "其他",
-            "lcde_b" : "銀行法人",
-            "lcde_c" : "大陸地區自然人",
-            "lcde_d" : "大陸地區法人"
-      }
+    "lcde_1": "本國人",
+    "lcde_2": "外國人",
+    "lcde_3": "國有",
+    "lcde_4": "省市",
+    "lcde_5": "縣市",
+    "lcde_6": "鄉鎮市",
+    "lcde_7": "本國私法人",
+    "lcde_8": "外國法人",
+    "lcde_9": "祭祀公業",
+    "lcde_a": "其他",
+    "lcde_b": "銀行法人",
+    "lcde_c": "大陸地區自然人",
+    "lcde_d": "大陸地區法人"
+}
 
 check_status_list = ['一般道路', '旱田', '道路相關設施', '未使用地']
+
 
 def get_lcdetype_flag(json_dict):
     str_ = ""
@@ -132,6 +133,59 @@ def get_land_info(get_payload):
     return l_area, l_price, t_value, l_owner.strip()
 
 
+def get_type(x_pos, y_pos):
+    url = f"https://luz.tcd.gov.tw/tcdgm/ws_identify.ashx?token=U65yWjl1zDnIAUMdC3gzyjERU-RhDGj4zNqwXSHvDooEsmBdJpu5ENgQGb8WfB2Y-f940BPEYguPgPxNzLUzHQ..&LAYERS=5%2C3%2C6&X={str(x_pos)}&Y={str(y_pos)}&Z=TWW"
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "cookie": "ExtentJS=24.194706399020145,120.69023354734935,19; ASP.NET_SessionId=nyymmbs0k4vphc0t51fdgqoy; _ga=GA1.3.1580569695.1680751244",
+        "referer": "https://nsp.tcd.gov.tw/tcdgm/Default.aspx",
+        "sec-ch-ua": "\"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+        "x-requested-with": "XMLHttpRequest"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            type_by_json = json.loads(response.text)
+            print(type_by_json)
+            if 'success' in type_by_json:
+                time.sleep(10)
+                response = requests.get(url, headers=headers)
+                type_by_json = json.loads(response.text)
+
+            result_head = type_by_json["5"]["features"]
+            if result_head:
+                result_body = type_by_json["5"]["features"][0]["properties"]
+                # print(f'Result: {result_body}')
+                type_use_area = result_body["使用分區"]
+                type_plan_name = result_body["計畫區名稱"]
+                print(f"使用分區: {type_use_area}")
+                print(f"計畫區名稱: {type_plan_name}")
+                return type_use_area, type_plan_name
+            else:
+                print("Empty type")
+                type_use_area = '主要幹道/國有/未分類'
+                type_plan_name = '臺中市都市計畫主要計畫'
+                return type_use_area, type_plan_name
+        else:
+            type_use_area = '失敗:未取得資訊'
+            type_plan_name = '失敗:未取得資訊'
+            print(f"Fail code：{response.status_code}")
+            return type_use_area, type_plan_name
+    except Exception as e:
+        raise e
+        type_use_area = f"{str(e)}, {x_pos}, {y_pos}"
+        type_plan_name = f"{str(e)}, {x_pos}, {y_pos}"
+        return type_use_area, type_plan_name
+
+
 def get_info(input_array, x_pos, y_pos):
     # '120.711271,24.160243' ;120.702043,24.170301
     # qryTile
@@ -160,7 +214,7 @@ def get_info(input_array, x_pos, y_pos):
         return input_array
     print(f'地段為:{sect}')
     print(f'地號為:{landno}')
-    sect_landno = sect+'-'+landno
+    sect_landno = sect + '-' + landno
     try:
         landno_array = np.array(input_array).T[1]
     except:
@@ -176,9 +230,10 @@ def get_info(input_array, x_pos, y_pos):
         # 土地調查
         land_area, land_price, total_value, land_owner = get_land_info(land_payload)
         score[land_status] = 1
+        use_area, plan_name = get_type(x_pos, y_pos)
         input_array.append(
             [decoded_str, sect_landno, own_area, land_status, land_area, land_price, total_value, land_owner,
-             np.round(x_pos, 6), np.round(y_pos, 6), score])
+             np.round(x_pos, 6), np.round(y_pos, 6), score, use_area, plan_name])
 
         print(f"面積為:{land_area} 平方公尺")
         print(f"單價為:{land_price} 元/平方公尺")
@@ -194,19 +249,18 @@ def get_info(input_array, x_pos, y_pos):
         print(np.where(landno_array == sect_landno))
         idx_check = np.where(landno_array == sect_landno)[0][0]
         own_area, land_status = location_query(query_pos)
-        if land_status not in input_array[idx_check][-1]:
-            input_array[idx_check][-1][land_status] = 1
+        if land_status not in input_array[idx_check][-3]:
+            input_array[idx_check][-3][land_status] = 1
         else:
-            input_array[idx_check][-1][land_status] += 1
-        max_score_land_status = max(input_array[idx_check][-1], key=input_array[idx_check][-1].get)
+            input_array[idx_check][-3][land_status] += 1
+        max_score_land_status = max(input_array[idx_check][-3], key=input_array[idx_check][-3].get)
         print(f'本次:{land_status}->最高分:{max_score_land_status}')
         input_array[idx_check][3] = max_score_land_status
         return input_array
-                     
-                  
+
 
 # data_set = {'地段名':[], '地段':[], '地號':[],'行政區':[], '狀態':[], '面積':[], '估值':[], '總值':[], '使用者':[]}
-col_names = ['地段名', '地段-地號', '行政區', '狀態', '面積', '估值', '總值', '使用者', '座標x', '座標y', 'score']
+col_names = ['地段名', '地段-地號', '行政區', '狀態', '面積', '估值', '總值', '使用者', '座標x', '座標y', 'score', '使用分區', '計畫區']
 # debug:120.721069	24.174339
 # x_min, x_max = 120.685709, 120.721948
 # y_min, y_max = 24.172339, 24.200506
